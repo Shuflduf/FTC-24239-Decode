965d014049c3101db14d41544caaa0e5f43d3298
Shuflduf <shuflduf@gmail.com>
2025-12-06 13:11:36 -0700

    upload 24239 to github

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/competition/ManualDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/competition/ManualDrive.java
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode.competition;
 
+import com.pedropathing.ftc.localization.Encoder;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
@@ -20,15 +23,7 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import java.util.List;
 @TeleOp(name="manual drive", group="competition")
 public class ManualDrive extends LinearOpMode{
-    private ElapsedTime runtime = new ElapsedTime();
-
-    //hardware variables
-    private DcMotor frontLeftDrive = null;
-    private DcMotor backLeftDrive = null;
-    private DcMotor frontRightDrive = null;
-    private DcMotor backRightDrive = null;
-    private DcMotor shooter = null;
-    private DcMotor intake = null;
+    private final ElapsedTime runtime = new ElapsedTime();
 
     //control variables
     public static final double SHOOTER_INTERVAL = 0.2;
@@ -43,28 +38,35 @@ public class ManualDrive extends LinearOpMode{
 //    private VisionPortal visionPortal;
 //    public static final double AUTO_TURN = 0.3;
 
-
+// 6 7 8 9 14
     @Override
     public void runOpMode() {
 
 //        initAprilTag();
         //shooter variables
-        boolean shooterToggle = false;
-        double shooterPower = 0;
+        boolean shooterEnabled = false;
+        boolean justChangedSpeed = false;
+//        double shooterPower = 0.2;
+        int speedIndex = 0;
+//        double lastPosition = 0.0;
+        double[] speeds = {0.5, 0.65, 0.7};
         //hardware assigning, make sure device names in here match the ones in config
-        frontLeftDrive = hardwareMap.get(DcMotor.class, "frontL");
-        backLeftDrive = hardwareMap.get(DcMotor.class, "backL");
-        frontRightDrive = hardwareMap.get(DcMotor.class, "frontR");
-        backRightDrive = hardwareMap.get(DcMotor.class, "backR");
-        shooter = hardwareMap.get(DcMotor.class, "shooter");
-        intake = hardwareMap.get(DcMotor.class, "intake");
+        //hardware variables
+        DcMotorEx frontLeftDrive = hardwareMap.get(DcMotorEx.class, "frontL");
+        DcMotorSimple backLeftDrive = hardwareMap.get(DcMotorSimple.class, "backL");
+        DcMotorEx frontRightDrive = hardwareMap.get(DcMotorEx.class, "frontR");
+        DcMotorEx backRightDrive = hardwareMap.get(DcMotorEx.class, "backR");
+        DcMotorEx shooter = hardwareMap.get(DcMotorEx.class, "shooter");
+        Servo intake = hardwareMap.get(Servo.class, "intake");
+
         //directions of wheels, may need to change directions to drive properly
-        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
-        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
-        frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
-        backRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        backRightDrive.setDirection(DcMotor.Direction.FORWARD);
         //direction of shooter
-        shooter.setDirection(DcMotor.Direction.REVERSE);
+        shooter.setDirection(DcMotor.Direction.FORWARD);
+
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -116,119 +118,83 @@ public class ManualDrive extends LinearOpMode{
                 backRightPower  /= max;
             }
 
+
+//            frontLeftPower = gamepad1.a ? 1.0 : 0.0;
+//            backLeftPower = gamepad1.b ? 1.0 : 0.0;
+//            frontRightPower = gamepad1.x ? 1.0 : 0.0;
+//            backRightPower = gamepad1.y ? 1.0 : 0.0;
+
             frontLeftDrive.setPower(frontLeftPower);
             frontRightDrive.setPower(frontRightPower);
             backLeftDrive.setPower(backLeftPower);
             backRightDrive.setPower(backRightPower);
 
             //shooter controls, allows for precise power setting mid match
-            if (gamepad1.right_bumper && !shooterToggle && shooterPower < 1) {
-                shooterPower += SHOOTER_INTERVAL;
-                shooter.setPower(shooterPower);
-                shooterToggle = true;
-            } else if (gamepad1.left_bumper && !shooterToggle && shooterPower > -0.2) {
-                shooterPower -= SHOOTER_INTERVAL;
-                shooter.setPower(shooterPower);
-                shooterToggle = true;
-            } else if (!gamepad1.right_bumper && !gamepad1.left_bumper) {
-                shooterToggle = false;
-            }
+//            if (gamepad1.right_bumper && !shooterToggle && shooterPower < 1) {
+//                shooterPower += SHOOTER_INTERVAL;
+//                shooter.setPower(shooterPower);
+//                shooterToggle = true;
+//            } else if (gamepad1.left_bumper && !shooterToggle && shooterPower > -0.2) {
+//                shooterPower -= SHOOTER_INTERVAL;
+//                shooter.setPower(shooterPower);
+//                shooterToggle = true;
+//            } else if (!gamepad1.right_bumper && !gamepad1.left_bumper) {
+//                shooterToggle = false;
+//            }
             //shooter controls to go right to zero or max
+
+            if (gamepad1.dpad_down && !justChangedSpeed) {
+//                shooterPower -= 0.05;
+//                shooterPower = Math.max(shooterPower, 0.2);
+                speedIndex -= 1;
+                speedIndex = Math.max(speedIndex, 0);
+                justChangedSpeed = true;
+            }
+            if (gamepad1.dpad_up && !justChangedSpeed) {
+//                shooterPower += 0.05;
+//                shooterPower = Math.min(shooterPower, 1.0);
+                speedIndex += 1;
+                speedIndex = Math.min(speedIndex, speeds.length - 1);
+                justChangedSpeed = true;
+            }
+            if (!gamepad1.dpad_up && !gamepad1.dpad_down) {
+                justChangedSpeed = false;
+            }
+
             if (gamepad1.right_trigger > 0.5){
-                shooterPower = 1;
-                shooter.setPower(shooterPower);
+                shooterEnabled = true;
+//                shooterPower = 1;
+//                shooter.setPower(shooterPower);
             }
             if (gamepad1.left_trigger > 0.5){
-                shooterPower = 0;
-                shooter.setPower(shooterPower);
+                shooterEnabled = false;
+            }
+            if (shooterEnabled) {
+//                double velocity = shooterEnc.getDeltaPosition() - lastPosition;
+                telemetry.addData("Shooter power:", "%4.2f", speeds[speedIndex]);
+//                lastPosition = shooterEnc.getDeltaPosition();
+
+                shooter.setPower(speeds[speedIndex]);
+            } else {
+                shooter.setPower(0.0);
+//                shooterEnc.setDirection(0.0);
             }
 
-            //intake controls
-            if (gamepad1.dpad_down){
-                intake.setPower(-0.5);
-            }else if (gamepad1.dpad_up){
-                intake.setPower(0.5);
-            }else if (gamepad1.dpad_right){
-                intake.setPower(0);
-            }else if (gamepad1.dpad_left){
-                intake.setPower(1);
-            };
 
-            //telemetry
-//            telemetryAprilTag();
+            //intake controls
+            if (gamepad1.left_bumper){
+                intake.setPosition(1.0);
+            }else if (gamepad1.right_bumper){
+                intake.setPosition(0.0);
+            }else{
+                intake.setPosition(0.5);
+            }
+
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Front left/Right", "%4.2f, %4.2f", frontLeftPower, frontRightPower);
             telemetry.addData("Back  left/Right", "%4.2f, %4.2f", backLeftPower, backRightPower);
-            telemetry.addData("Shooter power", shooterPower);
+            telemetry.addData("Shooter power", "%4.2f", speeds[speedIndex]);
             telemetry.update();
         }
     }
-
-    //april tag functions
-//    private void initAprilTag() {
-//
-//        aprilTag = new AprilTagProcessor.Builder()
-//
-//                .setDrawAxes(false)
-//                .setDrawCubeProjection(false)
-//                .setDrawTagOutline(true)
-//                .setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
-//                .setTagLibrary(AprilTagGameDatabase.getDecodeTagLibrary())
-//                .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
-//                .setCameraPose(cameraPosition, cameraOrientation)
-//
-//                .build();
-//
-//        VisionPortal.Builder builder = new VisionPortal.Builder();
-//
-//        if (USE_WEBCAM) {
-//            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
-//        } else {
-//            builder.setCamera(BuiltinCameraDirection.BACK);
-//        }
-//
-//        builder.enableLiveView(true);
-//
-//        builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
-//
-//        builder.setAutoStopLiveView(false);
-//
-//        builder.addProcessor(aprilTag);
-//
-//        visionPortal = builder.build();
-//
-//        visionPortal.setProcessorEnabled(aprilTag, true);
-//
-//    }
-//    private void telemetryAprilTag() {
-//
-//        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
-//        telemetry.addData("# AprilTags Detected", currentDetections.size());
-//
-//        for (AprilTagDetection detection : currentDetections) {
-//            if (detection.metadata != null) {
-//                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
-//                if (!detection.metadata.name.contains("Obelisk")) {
-//                    telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)",
-////                            detection.robotPose.getPosition().x,
-////                            detection.robotPose.getPosition().y,
-////                            detection.robotPose.getPosition().z));
-//                            detection.ftcPose.x,
-//                            detection.ftcPose.y,
-//                            detection.ftcPose.z));
-//                    telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)",
-//                            detection.robotPose.getOrientation().getPitch(AngleUnit.DEGREES),
-//                            detection.robotPose.getOrientation().getRoll(AngleUnit.DEGREES),
-//                            detection.robotPose.getOrientation().getYaw(AngleUnit.DEGREES)));
-//                }
-//            } else {
-//                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
-//                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
-//            }
-//        }
-//
-//        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
-//        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
-//
-//    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/competition/auto/drivethenshoot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/competition/auto/drivethenshoot.java
@@ -2,47 +2,63 @@ package org.firstinspires.ftc.teamcode.competition.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name = "drive", group = "competitive")
-public class justdrive extends LinearOpMode {
-    private ElapsedTime runtime = new ElapsedTime();
+@Autonomous(name = "drive then shoot", group = "competitive")
 
+public class drivethenshoot extends LinearOpMode {
     @Override
-    public void runOpMode(){
-        //hardware assigning, make sure device names in here match the ones in config
+    public void runOpMode() {
+        ElapsedTime runtime = new ElapsedTime();
+
         DcMotorEx frontLeftDrive = hardwareMap.get(DcMotorEx.class, "frontL");
         DcMotorSimple backLeftDrive = hardwareMap.get(DcMotorSimple.class, "backL");
         DcMotorEx frontRightDrive = hardwareMap.get(DcMotorEx.class, "frontR");
         DcMotorEx backRightDrive = hardwareMap.get(DcMotorEx.class, "backR");
-        //directions of wheels, may need to change directions to drive properly
+        DcMotorEx shooter = hardwareMap.get(DcMotorEx.class, "shooter");
+        Servo intake = hardwareMap.get(Servo.class, "intake");
+
         frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
         frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
         backRightDrive.setDirection(DcMotor.Direction.FORWARD);
-
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
+        shooter.setDirection(DcMotor.Direction.FORWARD);
 
         waitForStart();
         runtime.reset();
 
-        frontLeftDrive.setPower(1.0);
-        frontRightDrive.setPower(1.0);
-        backLeftDrive.setPower(5.0);
-        backRightDrive.setPower(1.0);
+        frontLeftDrive.setPower(-0.5);
+        frontRightDrive.setPower(-0.5);
+        backLeftDrive.setPower(-0.5);
+        backRightDrive.setPower(-0.5);
         runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < 1)) {
+        while (opModeIsActive() && (runtime.seconds() < 2)) {
             telemetry.addData("moving", "Leg 2: %4.1f S Elapsed", runtime.seconds());
             telemetry.update();
         }
+
         frontLeftDrive.setPower(0);
         frontRightDrive.setPower(0);
         backLeftDrive.setPower(0);
         backRightDrive.setPower(0);
+
+        shooter.setPower(0.55);
+        while (opModeIsActive() && (runtime.seconds() < 4)) {
+            telemetry.addData("revving", "%4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        intake.setPosition(1.0);
+        while (opModeIsActive() && (runtime.seconds() < 5)) {
+            telemetry.addData("shooting", "%4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        shooter.setPower(0.0);
+        intake.setPosition(0.5);
     }
 }
